8aec6df8901566ee0b32d1b9b57b72dd7491617c
AlexLE <alexle@live.com>
2013-07-10 23:28:48 -0700

    Changed ConnectionSettings class to expose properties of database object Modifying Connection Strings refreshes correctly in properties window Changes in Excel Exportation to avoid COM Exceptions

--- Core/Data/ConnectionSettings.cs
+++ Core/Data/ConnectionSettings.cs
@@ -9,29 +9,32 @@ using System.ComponentModel;
 
 namespace DataDevelop.Data
 {
-	[ReadOnly(true)]
-	public struct ConnectionSettings : IXmlSerializable
+	////[ReadOnly(true)]
+	public struct ConnectionSettings ////: IXmlSerializable
 	{
-		private string providerName;
-		private string databaseName;
-		private string connectionString;
+		private Database database;
+
+		public ConnectionSettings(Database database)
+		{
+			if (database == null) {
+				throw new ArgumentNullException("database");
+			}
+			this.database = database;
+		}
 
 		public string ProviderName
 		{
-			get { return this.providerName; }
-			set { this.providerName = value; }
+			get { return this.database.Provider.Name; }
 		}
 
 		public string DatabaseName
 		{
-			get { return this.databaseName; }
-			set { this.databaseName = value; }
+			get { return this.database.Name; }
 		}
 
 		public string ConnectionString
 		{
-			get { return this.connectionString; }
-			set { this.connectionString = value; }
+			get { return this.database.ConnectionString; }
 		}
 
 		public static bool operator ==(ConnectionSettings x, ConnectionSettings y)
@@ -49,32 +52,32 @@ namespace DataDevelop.Data
 			return base.GetHashCode();
 		}
 
-		#region IXmlSerializable Members
+		////#region IXmlSerializable Members
 
-		System.Xml.Schema.XmlSchema IXmlSerializable.GetSchema()
-		{
-			throw new NotImplementedException("The method or operation is not implemented.");
-		}
+		////System.Xml.Schema.XmlSchema IXmlSerializable.GetSchema()
+		////{
+		////    throw new NotImplementedException("The method or operation is not implemented.");
+		////}
 
-		public void ReadXml(XmlReader reader)
-		{
-			reader.ReadStartElement("ConnectionSettings");
-			this.databaseName = reader.GetAttribute("Name");
-			this.providerName = reader.GetAttribute("Provider");
-			this.connectionString = reader.ReadString();
-			reader.ReadEndElement();
-		}
+		////public void ReadXml(XmlReader reader)
+		////{
+		////    reader.ReadStartElement("ConnectionSettings");
+		////    this.databaseName = reader.GetAttribute("Name");
+		////    this.providerName = reader.GetAttribute("Provider");
+		////    this.connectionString = reader.ReadString();
+		////    reader.ReadEndElement();
+		////}
 
-		public void WriteXml(XmlWriter writer)
-		{
-			writer.WriteStartElement("ConnectionSettings");
-			writer.WriteAttributeString("Name", this.databaseName);
-			writer.WriteAttributeString("Provider", this.providerName);
-			writer.WriteString(this.connectionString);
-			writer.WriteEndElement();
-		}
+		////public void WriteXml(XmlWriter writer)
+		////{
+		////    writer.WriteStartElement("ConnectionSettings");
+		////    writer.WriteAttributeString("Name", this.databaseName);
+		////    writer.WriteAttributeString("Provider", this.providerName);
+		////    writer.WriteString(this.connectionString);
+		////    writer.WriteEndElement();
+		////}
 
-		#endregion
+		////#endregion
 
 		public override bool Equals(object obj)
 		{
@@ -86,9 +89,7 @@ namespace DataDevelop.Data
 
 		private static bool Equals(ConnectionSettings x, ConnectionSettings y)
 		{
-			return x.connectionString == y.connectionString
-				&& x.databaseName == y.databaseName
-				&& x.providerName == y.providerName;
+			return x.database == y.database;
 		}
 	}
 }
