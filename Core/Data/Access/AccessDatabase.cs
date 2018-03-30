6c4aff4e0b9d63fe389133e2cc404594c8b1a38e
Alex LE <alexle@live.com>
2018-03-30 15:55:16 -0700

    Access classes inherit from OleDb classes.

--- Core/Data/Access/AccessDatabase.cs
+++ Core/Data/Access/AccessDatabase.cs
@@ -1,145 +1,19 @@
-﻿using System;
-using System.Data;
-using System.Data.Common;
-using System.Data.OleDb;
+﻿using DataDevelop.Data.OleDb;
 
 namespace DataDevelop.Data.Access
 {
-	internal sealed class AccessDatabase : Database, IDisposable
+	internal sealed class AccessDatabase : OleDbDatabase
 	{
-		private string name;
-
 		public AccessDatabase(string name, string connectionString)
+			: base(name, connectionString)
 		{
-			this.name = name;
-			Connection = new OleDbConnection(connectionString);
 		}
-
-        public OleDbConnection Connection { get; }
-
-		public override string Name => name;
 
 		public override DbProvider Provider => AccessProvider.Instance;
 
-		public override string ConnectionString => Connection.ConnectionString;
-
-		public override DbDataAdapter CreateAdapter(Table table, TableFilter filter)
+		protected override OleDbTable CreateTableInstance(string schema, string name)
 		{
-			var accessTable = (AccessTable)table;
-			var adapter = new OleDbDataAdapter(table.GetBaseSelectCommandText(filter), Connection);
-			if (!table.IsReadOnly) {
-				var builder = new OleDbCommandBuilder(adapter) {
-					ConflictOption = ConflictOption.OverwriteChanges
-				};
-				try {
-					builder.GetUpdateCommand();
-				} catch (InvalidOperationException) {
-					accessTable.SetReadOnly(true);
-					return adapter;
-				}
-				adapter.DeleteCommand = builder.GetDeleteCommand();
-				adapter.UpdateCommand = builder.GetUpdateCommand();
-				adapter.InsertCommand = builder.GetInsertCommand();
-			}
-			return adapter;
-		}
-
-		public override DbCommand CreateCommand()
-		{
-			return Connection.CreateCommand();
-		}
-
-		public override DbTransaction BeginTransaction()
-		{
-			return Connection.BeginTransaction();
-		}
-
-		public override int ExecuteNonQuery(string commandText)
-		{
-			using (var command = Connection.CreateCommand()) {
-				command.CommandText = commandText;
-				return command.ExecuteNonQuery();
-			}
-		}
-
-		public override DataTable ExecuteTable(string commandText)
-		{
-			using (var command = Connection.CreateCommand()) {
-				command.CommandText = commandText;
-				var table = new DataTable();
-				using (var reader = command.ExecuteReader()) {
-					table.Load(reader);
-				}
-				return table;
-			}
-		}
-
-		public override int ExecuteNonQuery(string commandText, DbTransaction transaction)
-		{
-			if (!ReferenceEquals(transaction.Connection, Connection)) {
-				throw new InvalidOperationException();
-			}
-			using (var command = Connection.CreateCommand()) {
-				command.Transaction = (OleDbTransaction)transaction;
-				command.CommandText = commandText;
-				return command.ExecuteNonQuery();
-			}
-		}
-
-		#region IDisposable Members
-
-		public void Dispose()
-		{
-			if (Connection != null) {
-				Connection.Dispose();
-			}
-			GC.SuppressFinalize(this);
-		}
-
-		#endregion
-
-		public override void ChangeConnectionString(string newConnectionString)
-		{
-			if (IsConnected) {
-				throw new InvalidOperationException("Database must be disconnected in order to change the ConnectionString");
-			} else {
-				Connection.ConnectionString = newConnectionString;
-			}
-		}
-
-		protected override void DoConnect()
-		{
-			Connection.Open();
-		}
-
-		protected override void DoDisconnect()
-		{
-			Connection.Close();
-		}
-
-		protected override void PopulateTables(DbObjectCollection<Table> tablesCollection)
-		{
-			var tables = Connection.GetSchema("Tables", new[] { null, null, null, "Table" });
-			foreach (DataRow row in tables.Rows) {
-				var table = new AccessTable(this) {
-					Name = (string)row["TABLE_NAME"]
-				};
-				tablesCollection.Add(table);
-			}
-
-			var views = Connection.GetSchema("Tables", new[] { null, null, null, "View" });
-			foreach (DataRow row in views.Rows) {
-				var table = new AccessTable(this) {
-					Name = (string)row["TABLE_NAME"]
-				};
-				table.SetView(true);
-				tablesCollection.Add(table);
-			}
-		}
-
-		protected override void PopulateStoredProcedures(DbObjectCollection<StoredProcedure> storedProceduresCollection)
-		{
-			throw new NotSupportedException();
+			return new AccessTable(this) { Name = name };
 		}
 	}
 }
--- Core/Data/Access/AccessTable.cs
+++ Core/Data/Access/AccessTable.cs
@@ -1,80 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
 using System.Linq;
 using System.Text;
+using DataDevelop.Data.OleDb;
 
 namespace DataDevelop.Data.Access
 {
-	internal sealed class AccessTable : Table
+	internal sealed class AccessTable : OleDbTable
 	{
-		private bool isView;
-		private bool isReadOnly;
-
 		public AccessTable(AccessDatabase database)
 			: base(database)
 		{
 		}
 
-		public override bool IsView => isView;
-
-		public override bool IsReadOnly => isReadOnly;
-
-		private OleDbConnection Connection => ((AccessDatabase)Database).Connection;
-
-		private string[] GetPrimaryKey()
-		{
-			using (Database.CreateConnectionScope()) {
-				var schema = Connection.GetOleDbSchemaTable(OleDbSchemaGuid.Primary_Keys, new[] { null, null, Name });
-				var primaryKey = new string[schema.Rows.Count];
-				foreach (DataRowView row in schema.DefaultView) {
-					var i = Convert.ToInt32(row["ORDINAL"]) - 1;
-					primaryKey[i] = (string)row["COLUMN_NAME"];
-				}
-				return primaryKey;
-			}
-		}
-
-		public void SetView(bool value)
-		{
-			isView = value;
-		}
-
-		public void SetReadOnly(bool value)
-		{
-			isReadOnly = value;
-		}
-
-		public override bool Rename(string newName)
-		{
-			throw new NotImplementedException("The method or operation is not implemented.");
-		}
-
-		public override bool Delete()
-		{
-			throw new NotImplementedException("The method or operation is not implemented.");
-		}
-
-		public override int GetRowCount(TableFilter filter)
-		{
-			var count = -1;
-			var sql = new StringBuilder();
-			sql.Append("SELECT COUNT(*) FROM ");
-			sql.Append(QuotedName);
-
-			if (filter != null && filter.IsRowFiltered) {
-				sql.Append(" WHERE ");
-				filter.WriteWhereStatement(sql);
-			}
-			using (var command = Connection.CreateCommand()) {
-				command.CommandText = sql.ToString();
-				using (Database.CreateConnectionScope()) {
-					count = Convert.ToInt32(command.ExecuteScalar());
-				}
-			}
-			return count;
-		}
+		private new OleDbConnection Connection => ((AccessDatabase)Database).Connection;
 
 		public override DataTable GetData(int startIndex, int count, TableFilter filter, TableSort sort)
 		{
@@ -110,8 +50,8 @@ namespace DataDevelop.Data.Access
 				}
 			} else {
 				// SELECT TOP {count} * FROM 
-				//  (SELECT TOP {?} * FROM {table} ORDER BY {pk} ASC)
-				// ORDER BY {pk} DESC
+				//  (SELECT TOP {total - startIndex} * FROM {table} ORDER BY {pk} DESC)
+				// ORDER BY {pk} ASC
 				select.Append("SELECT TOP ");
 				select.Append(count);
 				select.Append(" * FROM (SELECT TOP ");
@@ -132,113 +72,6 @@ namespace DataDevelop.Data.Access
 			select.Append(" ORDER BY ");
 			select.Append(string.Join(", ", primaryKeyColumns.Select(i => i + " ASC").ToArray()));
 			return select.ToString();
-		}
-
-		private DataTable GetDataSecuencial(int startIndex, int count, TableFilter filter, TableSort sort)
-		{
-			var sql = new StringBuilder();
-			sql.Append("SELECT ");
-			filter.WriteColumnsProjection(sql);
-			sql.Append(" FROM ");
-			sql.Append(QuotedName);
-
-			if (filter != null && filter.IsRowFiltered) {
-				sql.Append(" WHERE ");
-				filter.WriteWhereStatement(sql);
-			}
-
-			if (sort != null && sort.IsSorted) {
-				sql.Append(" ORDER BY ");
-				sort.WriteOrderBy(sql);
-			}
-
-			var data = new DataTable(Name);
-			using (var select = Connection.CreateCommand()) {
-				select.CommandText = sql.ToString();
-
-				using (Database.CreateConnectionScope()) {
-					using (var reader = select.ExecuteReader(CommandBehavior.SequentialAccess)) {
-						for (var i = 0; i < reader.FieldCount; i++) {
-							data.Columns.Add(new DataColumn(reader.GetName(i), reader.GetFieldType(i)));
-						}
-
-						while (startIndex > 0 && reader.Read()) {
-							startIndex--;
-						}
-
-						if (reader.Read()) {
-							do {
-								var row = data.NewRow();
-								for (var i = 0; i < reader.FieldCount; i++) {
-									row[i] = reader[i];
-								}
-								data.Rows.Add(row);
-								count--;
-							} while (reader.Read() && count > 0);
-						}
-					}
-				}
-			}
-			data.AcceptChanges();
-			return data;
-		}
-
-		protected override void PopulateColumns(IList<Column> columnsCollection)
-		{
-			using (Database.CreateConnectionScope()) {
-				var columns = Connection.GetSchema("Columns", new[] { null, null, Name, null });
-				var rows = new DataRow[columns.Rows.Count];
-				foreach (DataRow row in columns.Rows) {
-					var i = Convert.ToInt32(row["ORDINAL_POSITION"]) - 1;
-					rows[i] = row;
-				}
-
-				var primaryKey = GetPrimaryKey();
-				foreach (var row in rows) {
-					var column = new Column(this) {
-						Name = row["COLUMN_NAME"].ToString()
-					};
-					if (primaryKey.Contains(column.Name, StringComparer.OrdinalIgnoreCase)) {
-						column.InPrimaryKey = true;
-					}
-					column.ProviderType = ((OleDbType)row["DATA_TYPE"]).ToString();
-					columnsCollection.Add(column);
-				}
-				SetColumnTypes(columnsCollection);
-			}
-		}
-
-		protected override void PopulateIndexes(IList<Index> indexesCollection)
-		{
-			using (Database.CreateConnectionScope()) {
-				var schema = Connection.GetOleDbSchemaTable(OleDbSchemaGuid.Indexes, null);
-				schema.DefaultView.Sort = "INDEX_NAME, ORDINAL_POSITION";
-				var indexes = new Dictionary<string, Index>();
-				foreach (DataRowView row in schema.DefaultView) {
-					if ((string)row["TABLE_NAME"] == Name) {
-						var indexName = (string)row["INDEX_NAME"];
-						if (!indexes.TryGetValue(indexName, out var index)) {
-							index = new Index(this, indexName) {
-								IsPrimaryKey = (bool)row["PRIMARY_KEY"],
-								IsUniqueKey = (bool)row["UNIQUE"]
-							};
-							indexes.Add(indexName, index);
-							indexesCollection.Add(index);
-						}
-						var columName = (string)row["COLUMN_NAME"];
-						var column = Columns.Single(i => i.Name == columName);
-						index.Columns.Add(new ColumnOrder(column));
-					}
-				}
-			}
-		}
-
-		protected override void PopulateForeignKeys(IList<ForeignKey> foreignKeysCollection)
-		{
-		}
-
-		protected override void PopulateTriggers(IList<Trigger> triggersCollection)
-		{
 		}
 	}
 }
