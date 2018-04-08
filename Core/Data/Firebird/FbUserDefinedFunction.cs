a8107f289155cd22a5ad66dbaee12c8f6078bbce
Alex LE <alexle@live.com>
2018-04-08 00:57:33 -0700

    Initial support for Firebird databases.

---
+++ Core/Data/Firebird/FbUserDefinedFunction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataDevelop.Data.Firebird
+{
+	class FbUserDefinedFunction : UserDefinedFunction
+	{
+		public FbUserDefinedFunction(FbDatabase database, string name)
+			: base(database)
+		{
+			Name = name;
+		}
+
+		public override string GenerateAlterStatement()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string GenerateCreateStatement()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string GenerateDropStatement()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string GenerateExecuteStatement()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void PopulateParameters(IList<Parameter> parametersCollection)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
