573cced4422bd769ec6a04a6fe447500f9a921ed
Alex LE <alexle@live.com>
2018-04-07 23:06:16 -0700

    Syntax

--- Core/Collections/ReadOnlyDictionary.cs
+++ Core/Collections/ReadOnlyDictionary.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections;
 
@@ -6,14 +6,12 @@ namespace DataDevelop.Collections
 {
 	public class ReadOnlyDictionary<TKey, TValue> : IDictionary<TKey, TValue>
 	{
-		private IDictionary<TKey, TValue> innerList;
+		private IDictionary<TKey, TValue> dict;
 
-		public ReadOnlyDictionary(IDictionary<TKey, TValue> list)
+		public ReadOnlyDictionary(IDictionary<TKey, TValue> dictionary)
 		{
-			this.innerList = list;
+			dict = dictionary;
 		}
-
-		#region IDictionary<TKey,TValue> Members
 
 		public void Add(TKey key, TValue value)
 		{
@@ -22,13 +20,11 @@ namespace DataDevelop.Collections
 
 		public bool ContainsKey(TKey key)
 		{
-			return innerList.ContainsKey(key);
+			return dict.ContainsKey(key);
 		}
 
-		public ICollection<TKey> Keys
-		{
-			get { return innerList.Keys; }
-		}
+		public ICollection<TKey> Keys => dict.Keys;
+
 
 		public bool Remove(TKey key)
 		{
@@ -37,23 +33,16 @@ namespace DataDevelop.Collections
 
 		public bool TryGetValue(TKey key, out TValue value)
 		{
-			return innerList.TryGetValue(key, out value);
+			return dict.TryGetValue(key, out value);
 		}
 
-		public ICollection<TValue> Values
-		{
-			get { return innerList.Values; }
-		}
+		public ICollection<TValue> Values => dict.Values;
 
 		public TValue this[TKey key]
 		{
-			get { return innerList[key]; }
-			set { throw new InvalidOperationException("This dictionary is read-only."); }
+			get => dict[key];
+			set => throw new InvalidOperationException("This dictionary is read-only.");
 		}
-
-		#endregion
-
-		#region ICollection<KeyValuePair<TKey,TValue>> Members
 
 		public void Add(KeyValuePair<TKey, TValue> item)
 		{
@@ -67,47 +56,31 @@ namespace DataDevelop.Collections
 
 		public bool Contains(KeyValuePair<TKey, TValue> item)
 		{
-			return innerList.Contains(item);
+			return dict.Contains(item);
 		}
 
 		public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
 		{
-			innerList.CopyTo(array, arrayIndex);
+			dict.CopyTo(array, arrayIndex);
 		}
 
-		public int Count
-		{
-			get { return innerList.Count; }
-		}
+		public int Count => dict.Count;
 
-		public bool IsReadOnly
-		{
-			get { return true; }
-		}
+		public bool IsReadOnly => true;
 
 		public bool Remove(KeyValuePair<TKey, TValue> item)
 		{
 			throw new InvalidOperationException("This dictionary is read-only.");
 		}
 
-		#endregion
-
-		#region IEnumerable<KeyValuePair<TKey,TValue>> Members
-
 		public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
 		{
-			return innerList.GetEnumerator();
+			return dict.GetEnumerator();
 		}
-
-		#endregion
-
-		#region IEnumerable Members
 
 		IEnumerator IEnumerable.GetEnumerator()
 		{
-			return innerList.GetEnumerator();
+			return dict.GetEnumerator();
 		}
-
-		#endregion
 	}
 }
