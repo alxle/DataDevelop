ba75148e82b4e539d5ae317b2d2b558968ef8d0d
Alex LE <alexle@live.com>
2022-07-04 22:07:33 -0700

    Syntax highlighting in Text Visualizer dialog.

--- DataDevelop/UIComponents/TextVisualizer.Designer.cs
+++ DataDevelop/UIComponents/TextVisualizer.Designer.cs
@@ -1,4 +1,4 @@
-namespace DataDevelop.UIComponents
+﻿namespace DataDevelop.UIComponents
 {
 	partial class TextVisualizer
 	{
@@ -27,8 +27,8 @@ namespace DataDevelop.UIComponents
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextVisualizer));
-			this.textBox = new System.Windows.Forms.TextBox();
 			this.toolStrip = new System.Windows.Forms.ToolStrip();
 			this.newToolStripButton = new System.Windows.Forms.ToolStripButton();
 			this.openToolStripButton = new System.Windows.Forms.ToolStripButton();
@@ -43,29 +43,24 @@ namespace DataDevelop.UIComponents
 			this.actionsToolStripDropDownButton = new System.Windows.Forms.ToolStripDropDownButton();
 			this.changeFontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.formatXmlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+			this.syntaxDropDownButton = new System.Windows.Forms.ToolStripDropDownButton();
+			this.plainTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+			this.htmlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.javaScriptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.sqlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.xmlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.statusStrip = new System.Windows.Forms.StatusStrip();
 			this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
 			this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
 			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
 			this.fontDialog = new System.Windows.Forms.FontDialog();
+			this.textEditor = new FastColoredTextBoxNS.FastColoredTextBox();
 			this.toolStrip.SuspendLayout();
 			this.statusStrip.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.textEditor)).BeginInit();
 			this.SuspendLayout();
-			// 
-			// textBox
-			// 
-			this.textBox.AcceptsReturn = true;
-			this.textBox.AcceptsTab = true;
-			this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.textBox.Location = new System.Drawing.Point(0, 25);
-			this.textBox.Multiline = true;
-			this.textBox.Name = "textBox";
-			this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.textBox.Size = new System.Drawing.Size(384, 319);
-			this.textBox.TabIndex = 0;
-			this.textBox.WordWrap = false;
-			this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
-			this.textBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
 			// 
 			// toolStrip
 			// 
@@ -80,10 +75,12 @@ namespace DataDevelop.UIComponents
             this.toolStripSeparator1,
             this.toggleWordWrapButton,
             this.toolStripSeparator2,
-            this.actionsToolStripDropDownButton});
+            this.actionsToolStripDropDownButton,
+            this.toolStripSeparator3,
+            this.syntaxDropDownButton});
 			this.toolStrip.Location = new System.Drawing.Point(0, 0);
 			this.toolStrip.Name = "toolStrip";
-			this.toolStrip.Size = new System.Drawing.Size(384, 25);
+			this.toolStrip.Size = new System.Drawing.Size(684, 25);
 			this.toolStrip.TabIndex = 3;
 			// 
 			// newToolStripButton
@@ -198,20 +195,82 @@ namespace DataDevelop.UIComponents
 			this.formatXmlToolStripMenuItem.Text = "Format XML";
 			this.formatXmlToolStripMenuItem.Click += new System.EventHandler(this.formatXmlToolStripMenuItem_Click);
 			// 
+			// toolStripSeparator3
+			// 
+			this.toolStripSeparator3.Name = "toolStripSeparator3";
+			this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+			// 
+			// syntaxDropDownButton
+			// 
+			this.syntaxDropDownButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.syntaxDropDownButton.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plainTextToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.htmlToolStripMenuItem,
+            this.javaScriptToolStripMenuItem,
+            this.sqlToolStripMenuItem,
+            this.xmlToolStripMenuItem});
+			this.syntaxDropDownButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.syntaxDropDownButton.Name = "syntaxDropDownButton";
+			this.syntaxDropDownButton.Size = new System.Drawing.Size(55, 22);
+			this.syntaxDropDownButton.Text = "Syntax";
+			// 
+			// plainTextToolStripMenuItem
+			// 
+			this.plainTextToolStripMenuItem.Checked = true;
+			this.plainTextToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.plainTextToolStripMenuItem.Name = "plainTextToolStripMenuItem";
+			this.plainTextToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+			this.plainTextToolStripMenuItem.Text = "Plain Text";
+			this.plainTextToolStripMenuItem.Click += new System.EventHandler(this.plainTextToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator4
+			// 
+			this.toolStripSeparator4.Name = "toolStripSeparator4";
+			this.toolStripSeparator4.Size = new System.Drawing.Size(123, 6);
+			// 
+			// htmlToolStripMenuItem
+			// 
+			this.htmlToolStripMenuItem.Name = "htmlToolStripMenuItem";
+			this.htmlToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+			this.htmlToolStripMenuItem.Text = "HTML";
+			this.htmlToolStripMenuItem.Click += new System.EventHandler(this.htmlToolStripMenuItem_Click);
+			// 
+			// javaScriptToolStripMenuItem
+			// 
+			this.javaScriptToolStripMenuItem.Name = "javaScriptToolStripMenuItem";
+			this.javaScriptToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+			this.javaScriptToolStripMenuItem.Text = "JavaScript";
+			this.javaScriptToolStripMenuItem.Click += new System.EventHandler(this.javaScriptToolStripMenuItem_Click);
+			// 
+			// sqlToolStripMenuItem
+			// 
+			this.sqlToolStripMenuItem.Name = "sqlToolStripMenuItem";
+			this.sqlToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+			this.sqlToolStripMenuItem.Text = "SQL";
+			this.sqlToolStripMenuItem.Click += new System.EventHandler(this.sqlToolStripMenuItem_Click);
+			// 
+			// xmlToolStripMenuItem
+			// 
+			this.xmlToolStripMenuItem.Name = "xmlToolStripMenuItem";
+			this.xmlToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+			this.xmlToolStripMenuItem.Text = "XML";
+			this.xmlToolStripMenuItem.Click += new System.EventHandler(this.xmlToolStripMenuItem_Click);
+			// 
 			// statusStrip
 			// 
 			this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.statusLabel});
-			this.statusStrip.Location = new System.Drawing.Point(0, 344);
+			this.statusStrip.Location = new System.Drawing.Point(0, 399);
 			this.statusStrip.Name = "statusStrip";
 			this.statusStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
-			this.statusStrip.Size = new System.Drawing.Size(384, 22);
+			this.statusStrip.Size = new System.Drawing.Size(684, 22);
 			this.statusStrip.TabIndex = 4;
 			// 
 			// statusLabel
 			// 
 			this.statusLabel.Name = "statusLabel";
-			this.statusLabel.Size = new System.Drawing.Size(369, 17);
+			this.statusLabel.Size = new System.Drawing.Size(669, 17);
 			this.statusLabel.Spring = true;
 			this.statusLabel.Text = "Text Length: 0";
 			// 
@@ -223,33 +282,66 @@ namespace DataDevelop.UIComponents
 			// 
 			this.openFileDialog.Title = "Open File...";
 			// 
+			// textEditor
+			// 
+			this.textEditor.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+			this.textEditor.AutoIndentExistingLines = false;
+			this.textEditor.AutoScrollMinSize = new System.Drawing.Size(27, 14);
+			this.textEditor.BackBrush = null;
+			this.textEditor.CharHeight = 14;
+			this.textEditor.CharWidth = 8;
+			this.textEditor.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.textEditor.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+			this.textEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textEditor.IsReplaceMode = false;
+			this.textEditor.Location = new System.Drawing.Point(0, 25);
+			this.textEditor.Name = "textEditor";
+			this.textEditor.Paddings = new System.Windows.Forms.Padding(0);
+			this.textEditor.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+			this.textEditor.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("textEditor.ServiceColors")));
+			this.textEditor.Size = new System.Drawing.Size(684, 374);
+			this.textEditor.TabIndex = 6;
+			this.textEditor.Zoom = 100;
+			// 
 			// TextVisualizer
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(384, 366);
-			this.Controls.Add(this.textBox);
+			this.ClientSize = new System.Drawing.Size(684, 421);
+			this.Controls.Add(this.textEditor);
 			this.Controls.Add(this.toolStrip);
 			this.Controls.Add(this.statusStrip);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+			this.KeyPreview = true;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "TextVisualizer";
 			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
 			this.Text = "Text Visualizer";
 			this.Load += new System.EventHandler(this.TextVisualizer_Load);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
 			this.toolStrip.ResumeLayout(false);
 			this.toolStrip.PerformLayout();
 			this.statusStrip.ResumeLayout(false);
 			this.statusStrip.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.textEditor)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
 		}
 
 		#endregion
-
-		private System.Windows.Forms.TextBox textBox;
 		private System.Windows.Forms.ToolStrip toolStrip;
 		private System.Windows.Forms.ToolStripButton newToolStripButton;
 		private System.Windows.Forms.ToolStripButton openToolStripButton;
@@ -269,5 +361,14 @@ namespace DataDevelop.UIComponents
 		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
 		private System.Windows.Forms.ToolStripMenuItem formatXmlToolStripMenuItem;
 		private System.Windows.Forms.FontDialog fontDialog;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+		private System.Windows.Forms.ToolStripDropDownButton syntaxDropDownButton;
+		private System.Windows.Forms.ToolStripMenuItem plainTextToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+		private System.Windows.Forms.ToolStripMenuItem htmlToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem javaScriptToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem sqlToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem xmlToolStripMenuItem;
+		private FastColoredTextBoxNS.FastColoredTextBox textEditor;
 	}
 }
--- DataDevelop/UIComponents/TextVisualizer.cs
+++ DataDevelop/UIComponents/TextVisualizer.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using System;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using System.Xml;
+using FastColoredTextBoxNS;
 
 namespace DataDevelop.UIComponents
 {
@@ -17,64 +15,90 @@ namespace DataDevelop.UIComponents
 		public TextVisualizer()
 		{
 			InitializeComponent();
-			this.textBox.Font = Properties.Settings.Default.TextVisualizerFont;
+			textEditor.Font = Properties.Settings.Default.TextVisualizerFont;
 		}
 
 		public bool TextValueChanged
 		{
-			get { return this.textChanged; }
+			get { return textChanged; }
 		}
 
 		public bool ReadOnly
 		{
-			get { return this.textBox.ReadOnly; }
+			get { return textEditor.ReadOnly; }
 			set
 			{
-				this.textBox.ReadOnly = value;
-				this.newToolStripButton.Enabled = !value;
-				this.openToolStripButton.Enabled = !value;
-				this.formatXmlToolStripMenuItem.Enabled = !value;
-				this.cutToolStripButton.Enabled = !value;
-				this.pasteToolStripButton.Enabled = !value;
+				textEditor.ReadOnly = value;
+				newToolStripButton.Enabled = !value;
+				openToolStripButton.Enabled = !value;
+				formatXmlToolStripMenuItem.Enabled = !value;
+				cutToolStripButton.Enabled = !value;
+				pasteToolStripButton.Enabled = !value;
 			}
 		}
 
 		public string TextValue
 		{
-			get { return this.textBox.Text; }
+			get { return textEditor.Text; }
 			set
 			{
-				this.textBox.Text = value;
-				this.textChanged = false;
+				textEditor.Text = value;
+				textChanged = false;
+				textEditor.ClearUndo();
+				if (value != null) {
+					DetectLanguage(value);
+				}
 			}
 		}
 
-		private void textBox_TextChanged(object sender, EventArgs e)
+		private void DetectLanguage(string text)
 		{
-			this.textChanged = true;
-			this.statusLabel.Text = "Text Length: " + this.textBox.TextLength;
+			var sqlRegex = new Regex(@"^\s*(SELECT|DECLARE|--+)\s+", RegexOptions.IgnoreCase);
+			if (sqlRegex.IsMatch(text)) {
+				sqlToolStripMenuItem.PerformClick();
+				return;
+			}
+			var xmlRegex = new Regex(@"\s*<xml\s+.*", RegexOptions.IgnoreCase);
+			if (xmlRegex.IsMatch(text)) {
+				xmlToolStripMenuItem.PerformClick();
+				return;
+			}
+			var htmlRegex = new Regex(@"^\s*(<html|<doctype)(>|\s).*", RegexOptions.IgnoreCase);
+			if (htmlRegex.IsMatch(text)) {
+				htmlToolStripMenuItem.PerformClick();
+				return;
+			}
+			if (Regex.IsMatch(text, @"^\s*<.+>\s*$", RegexOptions.Multiline)) {
+				xmlToolStripMenuItem.PerformClick();
+				return;
+			}
+		}
+
+		private void textEditor_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			textChanged = true;
+			statusLabel.Text = "Text Length: " + textEditor.TextLength;
 		}
 
 		private void TextVisualizer_Load(object sender, EventArgs e)
 		{
-			this.textBox.SelectionStart = 0;
-			this.textBox.SelectionLength = 0;
+			textEditor.SelectionStart = 0;
+			textEditor.SelectionLength = 0;
 		}
 
 		private void newToolStripButton_Click(object sender, EventArgs e)
 		{
-			this.textBox.Clear();
+			textEditor.ResetText();
 		}
 
 		private void openToolStripButton_Click(object sender, EventArgs e)
 		{
-			const long MaxLength = (2L * 1024 * 1024); // 2MB
+			const long MaxLength = (4L * 1024 * 1024); // 4MB
 			if (openFileDialog.ShowDialog(this) == DialogResult.OK) {
 				var info = new FileInfo(openFileDialog.FileName);
-				if (info.Length <= MaxLength) { 
-					this.textBox.Text = File.ReadAllText(info.FullName);
-				} else {
-					MessageBox.Show(this, "File Exceeds the Max Length allowed (2MB)");
+				if (info.Length <= MaxLength ||
+					MessageBox.Show(this, "Are you sure to open this large file?") == DialogResult.OK) {
+					textEditor.Text = File.ReadAllText(info.FullName);
 				}
 			}
 		}
@@ -82,36 +106,36 @@ namespace DataDevelop.UIComponents
 		private void saveToolStripButton_Click(object sender, EventArgs e)
 		{
 			if (saveFileDialog.ShowDialog(this) == DialogResult.OK) {
-				File.WriteAllText(saveFileDialog.FileName, this.textBox.Text);
+				File.WriteAllText(saveFileDialog.FileName, textEditor.Text);
 			}
 		}
 
 		private void cutToolStripButton_Click(object sender, EventArgs e)
 		{
-			this.textBox.Cut();
+			textEditor.Cut();
 		}
 
 		private void copyToolStripButton_Click(object sender, EventArgs e)
 		{
-			this.textBox.Copy();
+			textEditor.Copy();
 		}
 
 		private void pasteToolStripButton_Click(object sender, EventArgs e)
 		{
-			this.textBox.Paste();
+			textEditor.Paste();
 		}
 
 		private void toggleWordWrapButton_Click(object sender, EventArgs e)
 		{
-			this.textBox.WordWrap = toggleWordWrapButton.Checked;
+			textEditor.WordWrap = toggleWordWrapButton.Checked;
 		}
 
 		private void changeFontToolStripMenuItem_Click(object sender, EventArgs e)
 		{
-			this.fontDialog.Font = this.textBox.Font;
+			fontDialog.Font = textEditor.Font;
 			if (fontDialog.ShowDialog(this) == DialogResult.OK) {
-				this.textBox.Font = this.fontDialog.Font;
-				Properties.Settings.Default.TextVisualizerFont = this.fontDialog.Font;
+				textEditor.Font = fontDialog.Font;
+				Properties.Settings.Default.TextVisualizerFont = fontDialog.Font;
 				Properties.Settings.Default.Save();
 			}
 		}
@@ -119,31 +143,78 @@ namespace DataDevelop.UIComponents
 		private void formatXmlToolStripMenuItem_Click(object sender, EventArgs e)
 		{
 			try {
-				XmlDocument xd = new XmlDocument();
-				xd.LoadXml(this.textBox.Text);
-				StringBuilder sb = new StringBuilder();
-				StringWriter sw = new StringWriter(sb);
-				XmlTextWriter xtw = new XmlTextWriter(sw);
+				var xd = new XmlDocument();
+				xd.LoadXml(textEditor.Text);
+				var sb = new StringBuilder();
+				var sw = new StringWriter(sb);
+				var xtw = new XmlTextWriter(sw);
 				xtw.Formatting = Formatting.Indented;
 				xd.WriteTo(xtw);
 				xtw.Close();
-				this.textBox.Text = sb.ToString();
+				textEditor.Text = sb.ToString();
 			} catch (Exception ex) {
 				MessageBox.Show(this, ex.Message, "Error formatting XML", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
 		}
 
-		private void textBox_KeyDown(object sender, KeyEventArgs e)
+		private void Form_KeyDown(object sender, KeyEventArgs e)
 		{
 			if (e.Modifiers == Keys.Control && e.KeyCode == Keys.A) {
 				e.Handled = true;
 				e.SuppressKeyPress = true;
-				textBox.SelectAll();
+				textEditor.SelectAll();
 			} else if (e.Modifiers == Keys.None && e.KeyCode == Keys.Escape) {
 				e.Handled = true;
 				e.SuppressKeyPress = true;
-				this.Close();
+				Close();
 			}
+		}
+
+		private void SetLanguage(Language lang)
+		{
+			textEditor.ClearStylesBuffer();
+			textEditor.Range.ClearStyle(StyleIndex.All);
+			textEditor.Language = lang;
+			textEditor.OnSyntaxHighlight(new TextChangedEventArgs(textEditor.Range));
+		}
+
+		private void CheckSyntaxItem(ToolStripMenuItem sender)
+		{
+			foreach (var item in syntaxDropDownButton.DropDownItems) {
+				if (item is ToolStripMenuItem menuItem) {
+					menuItem.Checked = item == sender;
+				}
+			}
+		}
+
+		private void plainTextToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			SetLanguage(Language.Custom);
+			CheckSyntaxItem((ToolStripMenuItem)sender);
+		}
+
+		private void sqlToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			SetLanguage(Language.SQL);
+			CheckSyntaxItem((ToolStripMenuItem)sender);
+		}
+
+		private void xmlToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			SetLanguage(Language.XML);
+			CheckSyntaxItem((ToolStripMenuItem)sender);
+		}
+
+		private void htmlToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			SetLanguage(Language.HTML);
+			CheckSyntaxItem((ToolStripMenuItem)sender);
+		}
+
+		private void javaScriptToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			SetLanguage(Language.JS);
+			CheckSyntaxItem((ToolStripMenuItem)sender);
 		}
 	}
 }
